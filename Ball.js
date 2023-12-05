8740a0fd2f6cc1d25eb1400fe173c2392eefffc8
Reza <reza.amini.b@gmail.com>
2023-12-05 00:09:11 +0000

    - added get and set methods for the css properties for the ball class

--- Ball.js
+++ Ball.js
@@ -4,4 +4,20 @@ export default class Ball {
     constructor(ballElement){
         this.ballElement = ballElement;
     }
+
+    get x(){
+        return parseFloat(getComputedStyle(this.ballElement).getPropertyValue("--x"));
+    }
+
+    set x(value){
+        this.ballElement.style.setProperty("--x", value);
+    }
+
+    get y(){
+        return parseFloat(getComputedStyle(this.ballElement).getPropertyValue("--y"));
+    }
+
+    set y(value){
+        this.ballElement.style.setProperty("--y", value);
+    }
 }
