f8edc0250cf01509172e3e4693fc7d68e0482a09
Reza <reza.amini.b@gmail.com>
2023-12-08 01:06:42 +0000

    - increased the ball velocity on every update

--- Ball.js
+++ Ball.js
@@ -1,46 +1,54 @@
-const INITIAL_VELOCITY = .025;
+const INITIAL_VELOCITY = 0.025;
+const VELOCITY_INCREASE = 0.00001;
 
 export default class Ball {
-    constructor(ballElement){
-        this.ballElement = ballElement;
-        this.reset();
-    }
+  constructor(ballElement) {
+    this.ballElement = ballElement;
+    this.reset();
+  }
 
-    get x(){
-        return parseFloat(getComputedStyle(this.ballElement).getPropertyValue("--x"));
-    }
+  get x() {
+    return parseFloat(
+      getComputedStyle(this.ballElement).getPropertyValue("--x")
+    );
+  }
 
-    set x(value){
-        this.ballElement.style.setProperty("--x", value);
-    }
+  set x(value) {
+    this.ballElement.style.setProperty("--x", value);
+  }
 
-    get y(){
-        return parseFloat(getComputedStyle(this.ballElement).getPropertyValue("--y"));
-    }
+  get y() {
+    return parseFloat(
+      getComputedStyle(this.ballElement).getPropertyValue("--y")
+    );
+  }
 
-    set y(value){
-        this.ballElement.style.setProperty("--y", value);
-    }
+  set y(value) {
+    this.ballElement.style.setProperty("--y", value);
+  }
 
-    reset() {
-        this.x = 50;
-        this.y = 50;
-        this.direction = {x : 0};
+  reset() {
+    this.x = 50;
+    this.y = 50;
+    this.direction = { x: 0 };
 
-        while (Math.abs(this.direction.x <= .2) || Math.abs(this.direction.x >= .9)) {
-            
-            const heading = randomNumberBetween(0, 2* Math.PI);
-            this.direction = { x: Math.cos(heading), y: Math.sin(heading) }
-        }
-        this.velocity = INITIAL_VELOCITY;
+    while (
+      Math.abs(this.direction.x <= 0.2) ||
+      Math.abs(this.direction.x >= 0.9)
+    ) {
+      const heading = randomNumberBetween(0, 2 * Math.PI);
+      this.direction = { x: Math.cos(heading), y: Math.sin(heading) };
     }
+    this.velocity = INITIAL_VELOCITY;
+  }
 
-    update(delta){
-        this.x += this.direction.x * this.velocity * delta;
-        this.y += this.direction.y * this.velocity * delta;
-    }
+  update(delta, paddleRects) {
+    this.x += this.direction.x * this.velocity * delta;
+    this.y += this.direction.y * this.velocity * delta;
+    this.velocity += VELOCITY_INCREASE * delta;
+  }
 }
 
-function randomNumberBetween(min, max){
-    return Math.random() * (max - min) + min; 
+function randomNumberBetween(min, max) {
+  return Math.random() * (max - min) + min;
 }
