6deebd78e1db01cb44d55785d91acc40b49ea095
Reza <reza.amini.b@gmail.com>
2023-12-11 00:24:34 +0000

    - updated the player paddle position based on... mouse vertical direction movements

--- style.js
+++ style.js
@@ -1,6 +1,10 @@
 import Ball from "./Ball.js";
+import Paddle from "./Paddle.js";
 
 const ball = new Ball(document.getElementById("ball"));
+const playerPaddle = new Paddle(document.getElementById("player-paddle"));
+const computerPaddle = new Paddle(document.getElementById("computer-paddle"));
+
 
 let lastTime;
 function update(time) {
@@ -14,5 +18,10 @@ function update(time) {
 
   window.requestAnimationFrame(update);
 }
+
+
+document.addEventListener("mousemove", e => {
+  playerPaddle.position = (e.y / window.innerHeight) * 100;
+});
 
 window.requestAnimationFrame(update);
